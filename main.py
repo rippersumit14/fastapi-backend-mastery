1aed2475d5d6ec1e1e4c27552992adb8fce5836a
Sumit Pandey <131805506+rippersumit14@users.noreply.github.com>
2026-09-23 10:06:19 +0530

    Learned about the concept of HTTP exception, How we can add it to our route handlers and like learned the concept of query parameters and how query parameters can be helpful for filtering data search

--- main.py
+++ main.py
@@ -1,22 +1,70 @@
-from fastapi import FastAPI
+from typing import Any
+from fastapi import FastAPI,HTTPException, status
 from scalar_fastapi import get_scalar_api_reference
 
-app = FastAPI(); 
 
-@app.get("/shipment/{id}")
-def get_shipment(id: int) -> dict[str: str]: 
-    return {
-        "id": id,
-        "Item": "PS5",
-        "Status": "In Transit"
-    }
+app = FastAPI()
+
+shipments = {
+    12701: {
+        "weight": .6,
+        "content": "glassware",
+        "status": "placed"
+    },
+    12702: {
+        "weight": 2.3,
+        "content": "books",
+        "status": "shipped"
+    },
+    12703: {
+        "weight": 1.1,
+        "content": "electronics",
+        "status": "delivered"
+    },
+    12704: {
+        "weight": 3.5,
+        "content": "furniture",
+        "status": "in transit"
+    },
+    12705: {
+        "weight": .9,
+        "content": "clothing",
+        "status": "returned"
+    },
+    12706: {
+        "weight": 4.0,
+        "content": "appliances",
+        "status": "processing"
+    },
+    12707: {
+        "weight": 1.8,
+        "content": "toys",
+        "status": "placed"
+    },
+}
+
+@app.get("/shipment/latest",) #Static routes are always defined first 
+def get_latest_shipment() -> dict[str, Any]:
+    id = max(shipments.keys())
+    return shipments[id]
+
+@app.get("/shipment/{id}") #Dynamic routes are always created after the static route
+def get_shipment(id: int) -> dict[str, Any]:
+    if id not in shipments:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Given id does not exist"
+        )
     
+    return shipments[id]
+
+
+#Scalar API documentation
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
     return get_scalar_api_reference(
         openapi_url=app.openapi_url,
         title="Scalar API",
     )
-    
-    
+
 
